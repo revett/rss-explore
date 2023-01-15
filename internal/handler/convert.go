0a733c5033d57a117b6f7e27693dccc57791afaa
revett <2796074+revett@users.noreply.github.com>
2023-01-15 15:19:14 +0000

    Add logic to extract video ID from URL.

--- internal/handler/convert.go
+++ internal/handler/convert.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
+	"github.com/revett/rss-explore/internal/youtube"
 	"github.com/revett/rss-explore/pkg/api"
 )
 
@@ -22,8 +23,17 @@ func Convert(ctx echo.Context) error {
 		return returnErrorType(ctx, http.StatusBadRequest, "invalid request body")
 	}
 
-	if err := ctx.String(http.StatusOK, "rss-exploreeee"); err != nil {
-		return fmt.Errorf("sending string response: %w", err)
+	videoID, err := youtube.ExtractVideoID(body.URL)
+	if err != nil {
+		return returnErrorType(ctx, http.StatusBadRequest, "invalid youtube url")
+	}
+
+	resp := api.RSSFeed{
+		URL: videoID,
+	}
+
+	if err := ctx.JSON(http.StatusOK, resp); err != nil {
+		return fmt.Errorf("sending json response: %w", err)
 	}
 
 	return nil
---
+++ internal/youtube/extract_test.go
@@ -0,0 +1,47 @@
+package youtube_test
+
+import (
+	"testing"
+
+	"github.com/revett/rss-explore/internal/youtube"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFoo(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		url  string
+		want string
+	}{
+		"ValidLong": {
+			url:  "https://youtube.com/watch?v=7LICrnxWd38",
+			want: "7LICrnxWd38",
+		},
+		"ValidLongWithWWW": {
+			url:  "https://www.youtube.com/watch?v=7LICrnxWd38",
+			want: "7LICrnxWd38",
+		},
+		"ValidShort": {
+			url:  "https://youtu.be/7LICrnxWd38",
+			want: "7LICrnxWd38",
+		},
+		"ValidShortWithWWW": {
+			url:  "https://www.youtu.be/7LICrnxWd38",
+			want: "7LICrnxWd38",
+		},
+	}
+
+	for n, testCase := range tests {
+		tc := testCase
+
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+
+			videoID, err := youtube.ExtractVideoID(tc.url)
+
+			require.NoError(t, err)
+			require.Equal(t, tc.want, videoID)
+		})
+	}
+}
